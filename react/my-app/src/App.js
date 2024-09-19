bcf8747effbc280681548fde4d5bce0b19116b70
helloitsmeabhi <helloitsmeabhinav2003@gmail.com>
2024-09-19 20:33:43 +0530

    routes

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
-import  {Hello,ByeHello}  from './components/Hello';
+
+import { useState,useEffect } from 'react';
+import { BrowserRouter as Router,Route,Link, Routes  } from 'react-router-dom'
+import Contact from './components/Contact'
+import { Hello } from './components/Hello';
 function App() {
+  const [counter,setCounter] = useState(0);
+
+
+  const increment=()=>{
+    setCounter(counter+1)
+    
+  }
+  console.log(counter)
+  useEffect(()=>{
+    document.title=`clicked ${counter} times`
+  },[counter ])
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <p>Hello its me abhinav</p>
-      <Hello text="Hello how are u"/>
-      <ByeHello/>
+      <Router>
+      <nav>
+        <Link to='/' className='text'>Home</Link>
+        <Link to='/contact' className='text'>Contact</Link>
+        <Link to='/about' className='text'>About</Link>
+      </nav>
+
+      <button onClick={increment}>Click me</button>
+      <h1>{counter}</h1>
+      <Routes path='/contact' Component={Contact}/>
+      </Router>
     </div>
   );
 }
